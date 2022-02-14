c5c29a224ba0c6dc77e2f0037e8f325920286dfe
Ian Ault <ianault2022@isd282.org>
2022-02-14 11:34:34 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# filename          : main.py
+# description       : 
+# author            : Ian Ault
+# email             : ianault2022@isd282.org
+# date              : 02-14-2022
+# version           : v1.0
+# usage             : python main.py
+# notes             :
+# license           : MIT
+# py version        : 3.10.2 (must run on 3.6 or higher)
+#==============================================================================
